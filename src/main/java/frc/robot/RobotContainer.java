bc5262712e60f43d522a515ea1cfae54907a85ec
Nikhil <nsarathy3@gmail.com>
2026-09-22 19:55:29 -0500

    Fixed robotcontainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,12 +7,12 @@ package frc.robot;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.commands.ShootMove;
 import frc.robot.constants.GeneralConstants;
 import frc.robot.constants.TransferShootConstants;
 import frc.robot.constants.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Transfer;
 
 
@@ -22,11 +22,12 @@ public class RobotContainer {
   private final Transfer m_Transfer = new Transfer();
   private final Shooter m_Shooter = new Shooter();
 
+  CommandSwerveDrivetrain m_drivetrain = TunerConstants.createDrivetrain();
+
   //Controller(s)
   private final CommandXboxController m_driverController =
       new CommandXboxController(GeneralConstants.kDriverControllerPort);
 
-  ShootMove m_shootMove = new ShootMove(m_drivetrain, m_driverController);
 
     //Main robot Container constructor.
     public RobotContainer() {
@@ -53,7 +54,12 @@ public class RobotContainer {
 
     //Goes through shooting logic
 
-    m_driverController.rightBumper().whileTrue(m_Shooter.initShoot().finallyDo(() -> m_Shooter.stopShoot()));
+    m_driverController.rightBumper().whileTrue(
+      m_Shooter.shootSequenceCommand(
+        m_drivetrain, 
+        m_driverController.getRightY(),
+        m_driverController.getRightX()
+      ).finallyDo(() -> m_Shooter.stopShootMotors()));
     
 
 
